b0f8d156b7e7bc36ffc20c0943834d570a754bed
caglayurdasal <caglayurdasal@gmail.com>
2022-09-16 16:09:51 +0300

    Add solution file

---
+++ home/non_unique_elements.py
@@ -0,0 +1,27 @@
+def checkio(data: list) -> list:
+    # your code here
+    data_copy = []
+
+    for num in data:
+        if data.count(num) == 1:
+            del data[num]
+
+    return data_copy
+
+
+print("Example:")
+print(checkio([1, 2, 3, 1, 3]))
+
+assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+assert checkio([1, 2, 3, 4, 5]) == []
+assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+assert checkio([2, 2, 3, 2, 2]) == [2, 2, 2, 2]
+assert checkio([10, 20, 30, 10]) == [10, 10]
+assert checkio([7]) == []
+assert checkio([0, 1, 2, 3, 4, 0, 1, 2, 4]) == [0, 1, 2, 4, 0, 1, 2, 4]
+assert checkio([99, 98, 99]) == [99, 99]
+assert checkio([0, 0, 0, 1, 1, 100]) == [0, 0, 0, 1, 1]
+assert checkio([0, 0, 0, -1, -1, 100]) == [0, 0, 0, -1, -1]
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
